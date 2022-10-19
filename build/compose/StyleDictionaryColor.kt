a6793a0010316d029f5ad7eca97e56d852f00c2a
doe-jane <doe-jane@users.noreply.github.com>
2022-10-19 11:44:00 +0000

    Update design tokens

---
+++ build/compose/StyleDictionaryColor.kt
@@ -0,0 +1,77 @@
+
+
+// Do not edit directly
+// Generated on Wed, 19 Oct 2022 11:43:59 GMT
+
+
+
+package com.medium.android.design.theme;
+
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object MediumColorPalette {
+  val colorBaseBaseBlack = Color(0xff000000)
+  val colorBaseBaseWhite = Color(0xffffffff)
+  val colorBaseDanger100 = Color(0xfffee2e2)
+  val colorBaseDanger200 = Color(0xfffecaca)
+  val colorBaseDanger300 = Color(0xfffca5a5)
+  val colorBaseDanger400 = Color(0xfff87171)
+  val colorBaseDanger50 = Color(0xfffef2f2)
+  val colorBaseDanger500 = Color(0xffef4444)
+  val colorBaseDanger600 = Color(0xffdc2626)
+  val colorBaseDanger700 = Color(0xffb91c1c)
+  val colorBaseDanger800 = Color(0xff991b1b)
+  val colorBaseDanger900 = Color(0xff7f1d1d)
+  val colorBaseInformative100 = Color(0xffdbeafe)
+  val colorBaseInformative200 = Color(0xffbfdbfe)
+  val colorBaseInformative300 = Color(0xff93c5fd)
+  val colorBaseInformative400 = Color(0xff60a5fa)
+  val colorBaseInformative50 = Color(0xffeff6ff)
+  val colorBaseInformative500 = Color(0xff3b82f6)
+  val colorBaseInformative600 = Color(0xff2563eb)
+  val colorBaseInformative700 = Color(0xff1d4ed8)
+  val colorBaseInformative800 = Color(0xff1e40af)
+  val colorBaseInformative900 = Color(0xff1e3a8a)
+  val colorBaseNeutral100 = Color(0xfff1f5f9)
+  val colorBaseNeutral200 = Color(0xffe2e8f0)
+  val colorBaseNeutral300 = Color(0xffcbd5e1)
+  val colorBaseNeutral400 = Color(0xff94a3b8)
+  val colorBaseNeutral50 = Color(0xfff8fafc)
+  val colorBaseNeutral500 = Color(0xff64748b)
+  val colorBaseNeutral600 = Color(0xff475569)
+  val colorBaseNeutral700 = Color(0xff334155)
+  val colorBaseNeutral800 = Color(0xff1e293b)
+  val colorBaseNeutral900 = Color(0xff0f172a)
+  val colorBasePrimary100 = Color(0xffede9fe)
+  val colorBasePrimary200 = Color(0xffddd6fe)
+  val colorBasePrimary300 = Color(0xffc4b5fd)
+  val colorBasePrimary400 = Color(0xffa78bfa)
+  val colorBasePrimary50 = Color(0xfff5f3ff)
+  val colorBasePrimary500 = Color(0xff8b5cf6)
+  val colorBasePrimary600 = Color(0xff7c3aed)
+  val colorBasePrimary700 = Color(0xff6d28d9)
+  val colorBasePrimary800 = Color(0xff5b21b6)
+  val colorBasePrimary900 = Color(0xff4c1d95)
+  val colorBaseSuccess100 = Color(0xffdcfce7)
+  val colorBaseSuccess200 = Color(0xffbbf7d0)
+  val colorBaseSuccess300 = Color(0xff86efac)
+  val colorBaseSuccess400 = Color(0xff4ade80)
+  val colorBaseSuccess50 = Color(0xfff0fdf4)
+  val colorBaseSuccess500 = Color(0xff22c55e)
+  val colorBaseSuccess600 = Color(0xff16a34a)
+  val colorBaseSuccess700 = Color(0xff15803d)
+  val colorBaseSuccess800 = Color(0xff166534)
+  val colorBaseSuccess900 = Color(0xff14532d)
+  val colorBaseWarning100 = Color(0xfffef3c7)
+  val colorBaseWarning200 = Color(0xfffde68a)
+  val colorBaseWarning300 = Color(0xfffcd34d)
+  val colorBaseWarning400 = Color(0xfffbbf24)
+  val colorBaseWarning50 = Color(0xfffffbeb)
+  val colorBaseWarning500 = Color(0xfff59e0b)
+  val colorBaseWarning600 = Color(0xffd97706)
+  val colorBaseWarning700 = Color(0xffb45309)
+  val colorBaseWarning800 = Color(0xff92400e)
+  val colorBaseWarning900 = Color(0xff78350f)
+}
